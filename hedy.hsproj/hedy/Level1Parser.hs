e3976ca272810bcb580ac7c1fcf13406b83dd1b4
Mathias Verraes <mathias@verraes.net>
2020-04-06 16:20:05 +0200

    got the parser for level 1 working

---
+++ hedy.hsproj/hedy/Level1Parser.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE QuasiQuotes #-}
+module Level1Parser (parse) where
+    
+import Level1Lang
+import Text.RawString.QQ
+import Data.Void
+import Text.Megaparsec hiding (parse)
+import qualified Text.Megaparsec as M
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+
+type Parser = Parsec Void String
+
+parse :: String -> String -> Either (ParseErrorBundle String Void) Program
+parse = M.parse pAll
+
+sampleProgram' = [r|print Hello world!
+ask What is your name?
+echo Hi 
+|]
+
+pPrint :: Parser Stmt
+pPrint = do
+    keyword "print"
+    expr <- someTill printChar eol
+    return $ Print (Expr expr)
+    
+-- We could remove duplication here, but these will evolve separately anyway
+
+pAsk :: Parser Stmt
+pAsk = do
+    keyword "ask"
+    expr <- someTill printChar eol
+    return $ Ask (Expr expr)
+
+pEcho :: Parser Stmt
+pEcho = do
+    keyword "echo"
+    expr <- someTill printChar eol
+    return $ Echo (Expr expr)
+    
+pAll :: Parser Program
+pAll = do
+    sc -- for all space at the start of the file
+    program <- many (pPrint <|> pAsk <|> pEcho)
+    sc
+    eof
+    return program
+
+-- https://markkarpov.com/tutorial/megaparsec.html#lexing
+sc :: Parser ()
+sc = L.space
+  space1
+  (L.skipLineComment "//")
+  (L.skipBlockComment "/*" "*/")
+  
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+keyword :: String -> Parser String
+keyword s = lexeme (string s <* notFollowedBy alphaNumChar)
--- hedy.hsproj/hedy/Main.hs
+++ hedy.hsproj/hedy/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
 import Level1Lang
+import Level1Parser
 
-main = run sampleProgram
-        
+import Text.Megaparsec.Error
+import System.Environment
+import System.Exit
+
+main = getArgs >>= cmd
+
+cmd ["-h"] = usage   >> exitSuccess
+cmd ["-v"] = version >> exitSuccess
+cmd []     = usage   >> die "Missing file"
+cmd [filename] = do
+    body <- readFile filename
+    case (parse filename body) of 
+        Right program -> run program
+        Left errorBundle -> putStrLn $ errorBundlePretty errorBundle
+    
+
+usage   = putStrLn "Usage: hedy [-vh] [file]"
+version = putStrLn "Hedy 0.1"
+

a974b73868ebcca6014b52d3be20a418b335cb9f
Mathias Verraes <mathias@verraes.net>
2020-04-06 14:02:05 +0200

    init

---
+++ hedy.hsproj/hedy/Level1a.hs
@@ -0,0 +1,36 @@
+module Level1a (main') where
+    
+import Control.Monad.State.Lazy
+import System.Environment
+
+
+main' = evalStateT (execAll program) ""
+        
+program :: [Stmt]
+program = [ Print (Expr "Hello world!")
+          , Ask (Expr "What is your name")
+          , Echo (Expr "Hi ")
+          ]
+
+
+data Expr = Expr String deriving (Show)
+data Stmt = Print Expr
+            | Ask Expr
+            | Echo Expr
+            deriving (Show)
+    
+
+exec :: Stmt -> StateT String IO ()
+exec (Print (Expr s)) = do
+    lift $ putStrLn s
+exec (Ask (Expr s)) = do
+    lift $ putStrLn s
+    answer <- lift getLine
+    put answer
+exec (Echo (Expr s)) = do
+    answer <- get
+    lift $ putStrLn (s ++ answer)
+
+execAll :: [Stmt] -> StateT String IO ()
+execAll = mapM_ exec
+
---
+++ hedy.hsproj/hedy/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Level1a    
+
+main = main'
+        

f06b10647fea3644dd19e38d98c8926ef4e68d84
Mathias Verraes <mathias@verraes.net>
2020-04-09 21:45:04 +0200

    level 2 string interpolation tests

--- hedy.hsproj/hedy/Level2Spec.hs
+++ hedy.hsproj/hedy/Level2Spec.hs
@@ -16,8 +16,19 @@ spec = do
         it "replaces variables" $ do
             let vars = M.fromList [("name", "Hedy"), ("greeting", "Hi")]         
             replaceVars "greeting name!" vars `shouldBe` "Hi Hedy!"
+        
+        it "replaces variables that occur twice" $ do
+            let vars = M.fromList [("name", "Hedy")]         
+            replaceVars "name name" vars `shouldBe` "Hedy Hedy"  
 
-       
+        it "doesn't replaces variables recursively" $ do
+            let vars = M.fromList [("first", "second"), ("second", "third")]         
+            replaceVars "first second" vars `shouldBe` "second third"  
+            
+        it "doesn't replace substrings" $ do
+             let vars = M.fromList [("foo", "bar")]
+             replaceVars "foo shmfoo" vars `shouldBe` "bar shmfoo"  
+             replaceVars "foo shmfoo" vars `shouldNotBe` "bar shmbar"  
 
 simpleAST = 
     [ Assign "greeting" "Hello"

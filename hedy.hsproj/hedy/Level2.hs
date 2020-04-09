a69de9bded9a05b38c59db20ead86eef90e132b0
Mathias Verraes <mathias@verraes.net>
2020-04-09 21:47:41 +0200

    making it compile

--- hedy.hsproj/hedy/Level2.hs
+++ hedy.hsproj/hedy/Level2.hs
@@ -1,4 +1,4 @@
 module Level2 (module Level2Lang, module Level2Parser) where
     
-import Level1Lang
-import Level1Parser
+import Level2Lang
+import Level2Parser
---
+++ hedy.hsproj/hedy/Level2Parser.hs
@@ -0,0 +1,2 @@
+module Level2Parser where
+    
